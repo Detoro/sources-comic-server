7222bf72602d8b6e8fc9b1bed7bf2bba05a0b4db
detoro <batiu10@gmail.com>
2026-05-13 23:56:23 -0500

    removed mock data

--- core/src/commonMain/kotlin/models/Chapter.kt
+++ core/src/commonMain/kotlin/models/Chapter.kt
@@ -12,12 +12,3 @@ data class Chapter(
     val lastReadPageIndex: Int = 0,      // Crucial for bookmarking where the user left off
     val isDownloaded: Boolean = false    // True if the remote chapter was cached for offline reading
 )
-
-
-val mockChapters = listOf(
-    Chapter(id = "ch_1", comicId = "comic_1", chapterTitle = "Issue #1: Awakening", lastReadPageIndex = 12, isDownloaded = true),
-    Chapter(id = "ch_2", comicId = "comic_1", chapterTitle = "Issue #2: The Grid", lastReadPageIndex = 0, isDownloaded = false),
-    Chapter(id = "ch_3", comicId = "comic_1", chapterTitle = "Issue #3: Override", lastReadPageIndex = 0, isDownloaded = false),
-    Chapter(id = "ch_4", comicId = "comic_2", chapterTitle = "Volume 1: The Nebula", lastReadPageIndex = 0, isDownloaded = false),
-    Chapter(id = "ch_1", comicId = "comic_3", chapterTitle = "Volume 1: The Nebula", lastReadPageIndex = 50, isDownloaded = true)
-)
